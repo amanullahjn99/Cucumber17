d6332d875d120c22a5a2f2b7012c83b97f8cd43e
amanullahjn99 <amanullahjn99@gmail.com>
2023-11-27 22:15:57 -0500

    Nov27

--- src/test/java/utils/PageInitializer.java
+++ src/test/java/utils/PageInitializer.java
@@ -1,9 +1,7 @@
 package utils;
 
-import pages.AddEmployeePage;
-import pages.DashboardPage;
-import pages.EmployeeSearchPage;
-import pages.LoginPage;
+import pages.*;
+
 // we created to create the object and centralized the all the pages in one place
 public class PageInitializer {
 
@@ -12,12 +10,17 @@ public class PageInitializer {
     public static EmployeeSearchPage employeeSearchPage;
     public static AddEmployeePage addEmployeePage;
 
+    public static AdminDashBoardPage adminDashBoardPage;
+    public  static JobTitlePage jobTitlePage;
+
 
     public static void initializePageObjects(){
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
         employeeSearchPage = new EmployeeSearchPage();
         addEmployeePage = new AddEmployeePage();
+        adminDashBoardPage=new AdminDashBoardPage();
+        jobTitlePage=new JobTitlePage();
     }
 
 
