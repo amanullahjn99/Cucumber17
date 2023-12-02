414197bfe796ff304c6306eae19bd6a64d57aa27
amanullahjn99 <amanullahjn99@gmail.com>
2023-12-02 14:31:03 -0500

    Dec02

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -1,39 +1,110 @@
 package API;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.equalTo;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HardCodedExamples {
 
     //in rest assured base uri = base URL
     String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
-    String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDEzMTMzODIsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTcwMTM1NjU4MiwidXNlcklkIjoiNjA4NyJ9.eGoUkdW0fdPTMXXp59rLBQA8IV_-g4yRz8b4CDtcsFU";
+    String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDE1MzIwNTEsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTcwMTU3NTI1MSwidXNlcklkIjoiNjA4NyJ9.tGvmYYxGWpRXr3f3KejLdOTGwzLCrSWrMb4SgAkIGAY";
+    static String employee_id;
 
-    public void createEmployee(){
+    @Test
+    public void acreateEmployee(){
         //prepare the request
         //request specification allows us to prepare the request and gives it in variable
         RequestSpecification request = given().header("Content-Type","application/json").
                 header("Authorization", token)
                 .body("{\n" +
-                "  \"emp_firstname\": \"Amanl\",\n" +
-                "  \"emp_lastname\": \"Ahmadii\",\n" +
-                "  \"emp_middle_name\": \"None\",\n" +
-                "  \"emp_gender\": \"M\",\n" +
-                "  \"emp_birthday\": \"2000-11-26\",\n" +
-                "  \"emp_status\": \"Confirmed\",\n" +
-                "  \"emp_job_title\": \"QA\"\n" +
-                "}");
+                        "  \"emp_firstname\": \"hind\",\n" +
+                        "  \"emp_lastname\": \"pak\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"M\",\n" +
+                        "  \"emp_birthday\": \"2002-11-25\",\n" +
+                        "  \"emp_status\": \"confirmed\",\n" +
+                        "  \"emp_job_title\": \"qa\"\n" +
+                        "}");
 
         //send the request for creating the employee
         //response is the class which holds the complete response body and the info
         Response response =  request.when().post("/createEmployee.php");
 
-        //validate the response
+        //to print the response in console
+        response.prettyPrint();
+
+        //validate the response status
         response.then().assertThat().statusCode(201);
+        //validate the body
+        response.then().assertThat().
+                body("Message", equalTo("Employee Created"));
+        response.then().assertThat().
+                body("Employee.emp_firstname",equalTo("hind"));
+        response.then().assertThat().
+                header("Connection",equalTo("Keep-Alive"));
+        //to store the employee id after generating the employee
+        employee_id = response.jsonPath().getString("Employee.employee_id");
+        System.out.println(employee_id);
     }
 
+    @Test
+    public void bgetCreatedEmployee(){
+        //prepare the request
+        RequestSpecification request = given().
+                header("Content-Type","application/json").
+                header("Authorization", token).
+                queryParam("employee_id", employee_id);
+        Response response = request.when().get("/getOneEmployee.php");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+        //validate the employee id's one from post call another from get call
+        String tempEmpId = response.jsonPath().
+                getString("employee.employee_id");
+
+        Assert.assertEquals(tempEmpId, employee_id);
+    }
+
+    @Test
+    public void cUpdateEmployee(){
+        RequestSpecification request = given().
+                header("Content-Type","application/json").
+                header("Authorization", token).
+                body("{\n" +
+                        "  \"employee_id\": \""+employee_id+"\",\n" +
+                        "  \"emp_firstname\": \"rakhima\",\n" +
+                        "  \"emp_lastname\": \"bhujbal\",\n" +
+                        "  \"emp_middle_name\": \"simonov\",\n" +
+                        "  \"emp_gender\": \"F\",\n" +
+                        "  \"emp_birthday\": \"2001-11-09\",\n" +
+                        "  \"emp_status\": \"struggling\",\n" +
+                        "  \"emp_job_title\": \"survivor\"\n" +
+                        "}");
+        Response response = request.when().put("/updateEmployee.php");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void dgetupdatedEmployee(){
+        //prepare the request
+        RequestSpecification request = given().
+                header("Content-Type","application/json").
+                header("Authorization", token).
+                queryParam("employee_id", employee_id);
+        Response response = request.when().get("/getOneEmployee.php");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+        //validate the employee id's one from post call another from get call
+        String tempEmpId = response.jsonPath().
+                getString("employee.employee_id");
+        Assert.assertEquals(tempEmpId, employee_id);
+    }
 
 }
---
+++ src/test/java/APISteps/APIWorkFlowSteps.java
@@ -0,0 +1,133 @@
+package APISteps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class APIWorkFlowSteps {
+    String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+    public static String token;
+    RequestSpecification request;
+    Response response;
+    public static String employee_id;
+
+    @Given("a JWT is generated")
+    public void a_jwt_is_generated() {
+        RequestSpecification request = given().
+                header("Content-Type","application/json").
+                body("{\n" +
+                        "  \"email\": \"tests@batch17.com\",\n" +
+                        "  \"password\": \"Tests@123\"\n" +
+                        "}");
+        Response response = request.when().post("/generateToken.php");
+        //storing the token after generating it
+        token = "Bearer "+ response.jsonPath().getString("token");
+        System.out.println(token);
+    }
+
+    @Given("a request is prepared to create an employee")
+    public void a_request_is_prepared_to_create_an_employee() {
+        request = given().header("Content-Type","application/json").
+                header("Authorization", token)
+                .body("{\n" +
+                        "  \"emp_firstname\": \"hind\",\n" +
+                        "  \"emp_lastname\": \"pak\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"M\",\n" +
+                        "  \"emp_birthday\": \"2002-11-25\",\n" +
+                        "  \"emp_status\": \"confirmed\",\n" +
+                        "  \"emp_job_title\": \"qa\"\n" +
+                        "}");
+    }
+
+    @When("a POST call is made to create an employee")
+    public void a_post_call_is_made_to_create_an_employee() {
+        response =  request.when().post("/createEmployee.php");
+        //to print the response in console
+        response.prettyPrint();
+    }
+
+    @Then("the status code for this request is {int}")
+    public void the_status_code_for_this_request_is(Integer statusCode) {
+        response.then().assertThat().statusCode(statusCode);
+        //validate the body
+        response.then().assertThat().
+                body("Message", equalTo("Employee Created"));
+        response.then().assertThat().
+                body("Employee.emp_firstname",equalTo("hind"));
+        response.then().assertThat().
+                header("Connection",equalTo("Keep-Alive"));
+    }
+
+    @Then("the employee id {string} is stored as global variable for other request")
+    public void the_employee_id_is_stored_as_global_variable_for_other_request
+            (String empId) {
+        //empId is the parameter coming from feature file which is the path of employee id
+        employee_id = response.jsonPath().getString(empId);
+        System.out.println(employee_id);
+    }
+//------------------------------------------------------------------------------
+
+    @Given("a request is prepared to get the created employee")
+    public void a_request_is_prepared_to_get_the_created_employee() {
+        request = given().
+                header("Content-Type","application/json").
+                header("Authorization", token).
+                queryParam("employee_id", employee_id);
+    }
+
+    @When("a GET call is made to get the employee")
+    public void a_get_call_is_made_to_get_the_employee() {
+        response = request.when().get("/getOneEmployee.php");
+        response.prettyPrint();
+    }
+
+    @Then("the status code for this employee is {int}")
+    public void the_status_code_for_this_employee_is(Integer statusCode) {
+        response.then().assertThat().statusCode(statusCode);
+    }
+
+    @Then("the global employee id must match with {string} key")
+    public void the_global_employee_id_must_match_with_key(String empId) {
+        String tempEmpId = response.jsonPath().
+                getString(empId);
+        Assert.assertEquals(tempEmpId, employee_id);
+    }
+
+    @Then("the retrieved data at {string} object matches the data used to create employee")
+    public void the_retrieved_data_at_object_matches_the_data_used_to_create_employee
+            (String employeeObject, io.cucumber.datatable.DataTable dataTable) {
+        //one map comes from data table
+        List<Map<String, String>> expectedData = dataTable.asMaps();
+        //another map comes from employee object response
+        //get is going to return the whole map, getString returns just one value
+        Map<String, String> actualData =
+                response.body().jsonPath().get(employeeObject);
+
+        for (Map<String, String> map : expectedData){
+            //storing all the keys under set
+            Set<String> keys = map.keySet();
+            //from set of keys to one key at one time
+            for (String key:keys){
+                //this will return the value against the keys of datatable
+                String expectedValue = map.get(key);
+                //this will return the value against the employee object
+                String actualValue = actualData.get(key);
+                Assert.assertEquals(actualValue, expectedValue);
+            }
+        }
+
+    }
+
+}
